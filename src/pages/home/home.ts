33b44c85ba692e938be570d9a51ed00b51293c28
varsha bavane <varshab13bavane@gmail.com>
2018-04-06 09:24:15 +0530

    style changed of progress bar

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,18 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  tracks:any;
+  playing: boolean= true;
+  currentTrack :any;
+  progressInterval:any;
 
   constructor(public navCtrl: NavController) {
-
+    this.tracks=[
+      {title:"hi", artist:'odesza', playing:'false', progress:'0'},
+      {title:'hello', artist:'abcd', playing:'false', progress:'0'},
+      {title: 'HyperParadise', artist: 'Hermitude', playing: false, progress: 0},
+    ]
+  this.currentTrack=this.tracks[0];
   }
 
 }
--- src/pages/progress-bar/progress-bar.ts
+++ src/pages/progress-bar/progress-bar.ts
@@ -1,18 +1,16 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the ProgressBarPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+
 
 @Component({
   selector: 'page-progress-bar',
   templateUrl: 'progress-bar.html',
 })
 export class ProgressBarPage {
+  @Input('progress') progress;
+ 
+
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
